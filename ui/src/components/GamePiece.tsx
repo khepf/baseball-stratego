9d878cf2ae25ba70de9359ec99a3d700b691d80e
Jeremy Kepf <jeremy1975@gmail.com>
2026-01-13 16:47:23 -0500

    move pieceNumber to background

--- ui/src/components/GamePiece.tsx
+++ ui/src/components/GamePiece.tsx
@@ -24,24 +24,26 @@ export const GamePiece = ({
       }`}
     >
       {shouldReveal ? (
-        <div className="piece-content">
-          <div className="piece-rank">{piece.rank}</div>
-          <div className="piece-number">
+        <>
+          <div className="piece-number-bg">
             {piece.teamData.pieceNumberOrLetter}
           </div>
-          {piece.teamData.teamName && (
-            <>
-              <div className="piece-team">{piece.teamData.teamName}</div>
-              <div className="piece-year">{piece.teamData.year}</div>
-              <div className="piece-record">
-                {piece.teamData.wins}-{piece.teamData.losses}
-              </div>
-              <div className="piece-pct">
-                {piece.teamData.winPercentage.toFixed(3)}
-              </div>
-            </>
-          )}
-        </div>
+          <div className="piece-content">
+            <div className="piece-rank">{piece.rank}</div>
+            {piece.teamData.teamName && (
+              <>
+                <div className="piece-team">{piece.teamData.teamName}</div>
+                <div className="piece-year">{piece.teamData.year}</div>
+                <div className="piece-record">
+                  {piece.teamData.wins}-{piece.teamData.losses}
+                </div>
+                <div className="piece-pct">
+                  {piece.teamData.winPercentage.toFixed(3)}
+                </div>
+              </>
+            )}
+          </div>
+        </>
       ) : (
         <div className="piece-content hidden-content">
           <div className="piece-back">?</div>
