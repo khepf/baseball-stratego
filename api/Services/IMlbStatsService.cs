c20eb0addcc8e937ede61773805ac9aa2b368925
Jeremy Kepf <jeremy1975@gmail.com>
2026-01-16 15:44:04 -0500

    remove api

--- api/Services/IMlbStatsService.cs
+++
@@ -1,9 +0,0 @@
-using MLBApi.Models;
-
-namespace MLBApi.Services;
-
-public interface IMlbStatsService
-{
-    Task<List<TeamData>> GetRandomTeamsAsync(int count = 40);
-    Task<List<MlbTeam>> GetAllTeamsAsync();
-}
